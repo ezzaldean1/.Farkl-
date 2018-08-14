2a8b3b571278990be26533b437c346ab8596dd0b
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-15 00:00:02 +0300

    Update gan.js

--- gan.js
+++ gan.js
@@ -303,22 +303,21 @@ const Za7f = [
 ]
 
 
+
  client.on('message', message => {
      
      if (message.author.bot) return;
-   if (message.content.startsWith("عقاب")) {
+   if (message.content.startsWith("rعقاب")) {
                 if(!message.channel.guild) return message.reply('** This command only for servers**');
   var embed = new Discord.RichEmbed()
   .setColor('RANDOM')
-   .addField('Nabarat Games' ,
    .setThumbnail(message.author.avatarURL) 
+   .addField('Nabarat Games' ,
   `${Za7f[Math.floor(Math.random() * Za7f.length)]}`)
   message.channel.sendEmbed(embed);
   console.log('[38ab] Send By: ' + message.author.username)
     }
 });
-
-
 
 var rebel = ["https://f.top4top.net/p_682it2tg6.png","https://e.top4top.net/p_682a1cus5.png","https://d.top4top.net/p_682pycol4.png","https://c.top4top.net/p_682vqehy3.png","https://b.top4top.net/p_682mlf9d2.png","https://a.top4top.net/p_6827dule1.png","https://b.top4top.net/p_682g1meb10.png","https://a.top4top.net/p_682jgp4v9.png","https://f.top4top.net/p_682d4joq8.png","https://e.top4top.net/p_6828o0e47.png","https://d.top4top.net/p_6824x7sy6.png","https://c.top4top.net/p_682gzo2l5.png","https://b.top4top.net/p_68295qg04.png","https://a.top4top.net/p_682zrz6h3.png","https://f.top4top.net/p_6828vkzc2.png","https://e.top4top.net/p_682i8tb11.png"]
     client.on('message', message => {
